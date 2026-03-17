4e222cc0b318c12fe5e0c61d1c7632b762cd0d77
victory <nwanoruovictory0@gmail.com>
2026-03-17 07:47:34 +0000

    upadte intitialized react router for multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import "./index.css";
 import Home from "./component/Home/Home";
 import ProductDetails from "./component/Product Details/ProductDetails";
 import ProductCategory from "./component/ProductCartegoty/ProductsCartegory";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <ProductDetails />,
+      },
+    ],
+    {
+      basename: "/Electronic-Store/",
+    },
+  );
+
   return (
     <>
-      <ProductCategory />
+      <RouterProvider router={router} />
     </>
   );
 }
